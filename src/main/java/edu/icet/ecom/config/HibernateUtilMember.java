4dc244bce9a31e0bf09526c281118cb47a2a2056
UdaraDewshan <dewudara@gmail.com>
2025-11-23 02:00:34 +0530

    Add HibernateUtilMember singleton configuration class in Member

--- src/main/java/edu/icet/ecom/config/HibernateUtilMember.java
+++ src/main/java/edu/icet/ecom/config/HibernateUtilMember.java
@@ -1,4 +1,21 @@
 package edu.icet.ecom.config;
 
+import edu.icet.ecom.model.entity.Member;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtilMember {
+    private static SessionFactory sessionFactory;
+    private static Configuration configuration;
+
+    public static SessionFactory getSessionFactory(){
+        if (sessionFactory==null){
+            configuration = new Configuration();
+            configuration.addAnnotatedClass(Member.class);
+            configuration.configure("hibernate.cfg.xml");
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
