33dd89ce05e7687b3d08b773db06bcc5331f0a01
UdaraDewshan <dewudara@gmail.com>
2025-11-22 21:57:34 +0530

    Create a Member class

--- src/main/java/edu/icet/ecom/model/entity/Member.java
+++ src/main/java/edu/icet/ecom/model/entity/Member.java
@@ -1,4 +1,16 @@
 package edu.icet.ecom.model.entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Member {
+    private String id;
+    private String name;
+    private String address;
+    private String phoneNo;
 }
