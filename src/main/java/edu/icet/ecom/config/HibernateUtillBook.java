ada03cfc9bd19a87afd8bfe24745c3afe959a14f
UdaraDewshan <dewudara@gmail.com>
2025-11-23 23:36:12 +0530

    Add HibernateUtilBook singleton configuration class

--- src/main/java/edu/icet/ecom/config/HibernateUtillBook.java
+++ src/main/java/edu/icet/ecom/config/HibernateUtillBook.java
@@ -1,5 +1,23 @@
 package edu.icet.ecom.config;
 
+import edu.icet.ecom.model.entity.Book;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtillBook {
+
+    private static SessionFactory sessionFactory;
+    private HibernateUtillBook(){}
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory==null){
+            Configuration configuration = new Configuration();
+            configuration.addAnnotatedClass(Book.class);
+            configuration.configure("hibernate.cfg.xml");
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
 
 }
